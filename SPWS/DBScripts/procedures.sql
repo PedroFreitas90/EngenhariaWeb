c9cdda6a20d5ab7498de66ff1744b397f7a94008
FranciscoFreitas45 <a81580@alunos.uminho.pt>
2020-04-04 22:34:52 +0100

    modificacao na bd

--- SPWS/DBScripts/procedures.sql
+++ SPWS/DBScripts/procedures.sql
@@ -1,18 +1,15 @@
-CREATE DEFINER=`root`@`localhost` FUNCTION `distance`(latA float,latB float,longA float,longB float) RETURNS float
+DELIMITER $$
+CREATE  FUNCTION distance (latA float,latB float,longA float,longB float) RETURNS float
 BEGIN
 Declare distance float;
    select ST_Distance_Sphere(
     point(longA,latA),
     point(longB,latB)
-) into distance;   
+) into distance;
 RETURN distance;
 END
-
-
-
-
-
-
+$$
+DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `closerCar`(in cross_id int ,in car_id int )
 BEGIN
 
@@ -27,26 +24,43 @@ select c.latitude into @latB from crosswalk c where c.oid =cross_id;
 select c.longitude into @longB from crosswalk c where c.oid =cross_id;
 
 select `SPWS`.`distance`(@latA,@latB,@longA,@longB) into @distance;
-	
+
     if(@distance <10) then
     Start  Transaction;
     INSERT INTO crosswalk_car(crosswalk_oid,car_oid,distance,day)
 	VALUES(cross_id,car_id,@distance,current_date());
-	
+
     if Erro
     then rollback;
     else commit;
     end if;
 
     start transaction;
-    INSERT INTO historic_car(day,crosswalk_oid,car_oid)
-    VALUES(current_date(),cross_id,car_id);
+    SELECT Count(*) into @exist from historic where day = current_date() and crosswalk_oid = cross_id;
+    SELECT Count(*) INTO @size from historic;
+    SELECT Count(*) into @size2 from historic_car;
+
+
+    SET @size2 = @size2 +1;
+
+      IF @exist=0 THEN
+      SET @size = @size +1;
+      INSERT INTO historic(oid,day,crosswalk_oid) VALUES(@size,current_date(),cross_id);
+      END if ;
+
+SELECT oid into @oid from historic where day = current_date() and crosswalk_oid = cross_id;
+select Count(*) into @exist2 from historic_car where car_oid =car_id and historic_oid = @oid;
+
+      IF @exist2 = 0 then
+    INSERT INTO historic_car(oid,historic_oid,car_oid) VALUES(@size2,@oid,car_id);
+    END IF;
     if Erro
     then rollback;
     else commit;
     end if;
 end if;
 end
+$$
 
 
 
@@ -59,10 +73,7 @@ end
 
 
 
-
-
-
-
+DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `closerPedestrian`(in cross_id int ,in ped_id int )
 BEGIN
 
@@ -77,31 +88,46 @@ select c.latitude into @latB from crosswalk c where c.oid =cross_id;
 select c.longitude into @longB from crosswalk c where c.oid =cross_id;
 
 select `SPWS`.`distance`(@latA,@latB,@longA,@longB) into @distance;
-	
+
     if(@distance <10) then
     Start  Transaction;
     INSERT INTO pedestrian_crosswalk(pedestrian_oid,crosswalk_oid,distance,day)
 	VALUES(ped_id,cross_id,@distance,current_date());
-	
+
     if Erro
     then rollback;
     else commit;
     end if;
 start transaction;
-    INSERT INTO historic_pedestrian(day,crosswalk_oid,pedestrian_oid)
-    VALUES(current_date(),cross_id,ped_id);
+
+SELECT Count(*) into @exist from historic where day = current_date() and crosswalk_oid = cross_id;
+SELECT Count(*) INTO @size from historic;
+SELECT Count(*) into @size2 from historic_pedestrian;
+
+
+SET @size2 = @size2 +1;
+
+  IF @exist=0 THEN
+  SET @size = @size +1;
+  INSERT INTO historic(oid,day,crosswalk_oid) VALUES(@size,current_date(),cross_id);
+  END IF ;
+
+SELECT oid into @oid from historic where day = current_date() and crosswalk_oid = cross_id ;
+select Count(*) into @exist2 from historic_pedestrian where pedestrian_oid = ped_id and historic_oid = @oid;
+
+	IF @exist2 = 0 then
+		INSERT INTO historic_pedestrian(oid,pedestrian_oid,historic_oid) VALUES(@size2,ped_id,@oid);
+	END IF;
     if Erro
     then rollback;
     else commit;
     end if;
 end if;
 end
+$$
 
-
-
-
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `closerCars`(in cross_id int)
+DELIMITER $$
+CREATE  PROCEDURE closerCars(in cross_id int)
 BEGIN
 DECLARE x  int default 1;
 
@@ -110,20 +136,21 @@ truncate table crosswalk_car;
 select count(*) into @countC from car;
 
 	cross_loop: LOOP
-    
+
 		IF @countC < x then
 			leave cross_loop;
         end if;
        call closerCar(cross_id,x);
-       
+
        SET x= x+1;
-       
+
        end loop;
-       
+
 END
 
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `closerPedestrians`(in cross_id int)
+$$
+DELIMITER $$
+CREATE PROCEDURE closerPedestrians (in cross_id int)
 BEGIN
 DECLARE x  int default 1;
 
@@ -132,21 +159,15 @@ truncate table pedestrian_crosswalk;
 select count(*) into @countC from pedestrian;
 
 	cross_loop: LOOP
-    
+
 		IF @countC < x then
 			leave cross_loop;
         end if;
        call closerPedestrian(cross_id,x);
-       
+
        SET x= x+1;
-       
+
        end loop;
-       
+
 END
-
-
-
-
-
-
-
+$$
