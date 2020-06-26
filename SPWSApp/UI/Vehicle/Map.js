afe0442f05eee4f086a6225826a8e807c6de436b
Pedro Freitas <pedrofreitas1410@gmail.com>
2020-06-26 20:30:34 +0100

    Remover chaves do pubnub

--- SPWSApp/UI/Vehicle/Map.js
+++ SPWSApp/UI/Vehicle/Map.js
@@ -13,8 +13,8 @@ const uuid = PubNub.generateUUID();
 const identifier = Constants.installationId
 
 const pubnub = new PubNub({
-  publishKey: "pub-c-627ad9bf-507f-4fae-9a30-6fb40d8eff88",
-  subscribeKey: "sub-c-2bc1178c-8e5e-11ea-927a-2efbc014b69f",
+  publishKey: /* É preciso uma chave de pubnub*/,
+  subscribeKey: /* É preciso uma chave de pubnub*/,
   uuid: uuid,
   subscribeRequestTimeout: 60000,
   presenceTimeout: 122,
